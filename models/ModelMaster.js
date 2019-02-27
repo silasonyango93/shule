b3d94b6ff84316bc739bc63751adb4bd295920e4
silasonyango93 <silas.onyango93@gmail.com>
2019-02-27 14:03:28 +0300

    Partial curriculum environment preparation

--- models/ModelMaster.js
+++ models/ModelMaster.js
@@ -47,7 +47,7 @@ database.
 		  con.query('INSERT INTO ' + tableName + ' SET ?', jsonObject_, function (err, result) {
             if (err){reject(err);}
                else {
-				   var returned_value_={success:true, message:"Record inserted succesfully."};
+				   var returned_value_={success:true, message:"Record inserted succesfully.",recordId:result.insertId};
                    resolve(returned_value_);
                     }
             
